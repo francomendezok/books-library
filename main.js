6379483f1a7feb7aefadeff952c858a0d2b6e6a3
francomendezok <125481413+francomendezok@users.noreply.github.com>
2023-07-03 11:59:18 -0300

    Basic Boilerplate

---
+++ main.js
@@ -0,0 +1,32 @@
+let myLibrary = [];
+let quotes = [
+    "If you need a degree to do it, it's not going to make you wealthy.",
+    "University degrees are the new taxi medallions.",
+    "The overeducated are worse off than the undereducated, having traded common sense for the illusion of knowledge.",
+    "If the primary purpose of school was education, the Internet should obsolete it. But school is mainly about credentialing.",
+    "Teaching is more a way for the teacher to learn than for the student to learn.",
+    "The skills you really want can’t be taught, but they can be learned.",
+    "Free education is abundant, all over the Internet. It's the desire to learn that's scarce.",
+    "It's not about 'educated' v 'un-educated.' It's about 'likes to read' and 'doesn't like to read.'",
+    "The Internet will obsolete the industrial education system, just like it's obsoleting every other physical purveyor of information goods.",
+    "The smarter you get, the slower you read.",
+    "If you can speed read it, it isn't worth reading.",
+    "A vacation is a very expensive way to schedule the time to read a book in peace.",
+    "Read what you love until you love to read.",
+    "If they wrote it to make money, don't read it.",
+    "Study logic and math, because once you've mastered them, you won't fear any book.",
+    "Learn to love to read, and all human knowledge is available to you right now. No need for a priest to interpret a textbook for you.",
+    "Reading a book isn't a race - the better the book, the slower it should be absorbed."
+]
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function randomQuote () {
+
+}
